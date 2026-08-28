23b15f9ff9b351abf11d0f08337767f8719b6c53
MuhammadAmmarKhan <154527250+MuhammadAmmarKhan@users.noreply.github.com>
2026-08-29 07:28:15 +1000

    URL FIX

--- includes/partials/app.php
+++ includes/partials/app.php
@@ -4,7 +4,7 @@ define('BASE_URL', $protocol . $host . '/');
 function asset($path)
 {
     $localPath = __DIR__ . '/smashapparal/assets/' . $path;
-    $baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/assets/";
+    $baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/smashapparal/assets/";
     $version = '1.8.4';
     if (file_exists($localPath)) {
         $version = filemtime($localPath);
